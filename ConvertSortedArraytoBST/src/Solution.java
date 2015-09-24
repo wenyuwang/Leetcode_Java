e8d3a3c34a903b898a371c0c898f33cf04a5083c
Wen-Yu, Wang <ww424@cornell.edu>
2015-09-24 10:39:19 -0400

    Add Binary Tree Right Side View Part

--- ConvertSortedArraytoBST/src/Solution.java
+++ ConvertSortedArraytoBST/src/Solution.java
@@ -1,3 +1,6 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Solution {
     public TreeNode2 sortedArrayToBST(int[] nums) {
@@ -18,4 +21,25 @@ public class Solution {
 		return root;
 		}
     
+
+    
+    public ArrayList<Integer> rightSideView(TreeNode2 root) {
+    	ArrayList<Integer> ret = new ArrayList<Integer>();
+    	if (root == null) return ret;
+    	
+    	LinkedList<TreeNode2> queue = new LinkedList<TreeNode2>();
+    	queue.add(root);
+    	while(queue.size() != 0){
+    		int size = queue.size();
+    		for (int i =0; i<size; i++){
+    			TreeNode2 top = queue.remove();
+    			if(i ==0 ) ret.add(top.val);
+    			if(top.right != null) queue.add(top.right);
+    			if(top.left != null) queue.add(top.left);
+    		}
+    	}
+    	
+        return ret;
+        
+    }
 }
