dc5ab939d4144d85085b40b3babc8f0fd41a77d4
Wen-Yu, Wang <ww424@cornell.edu>
2015-10-05 22:52:24 -0400

    invert tree

--- TreeInLeetCode/src/Solution.java
+++ TreeInLeetCode/src/Solution.java
@@ -20,7 +20,16 @@ public class Solution {
                 isSameTree(p.right, q.right) && isSameTree(p.left, q.left));
         
     }
-	
+    
+    public TreeNode invertTree(TreeNode root) {
+    	if(root == null) return root;
+    	TreeNode left = root.left;
+    	root.left = root.right;
+    	root.right = left;
+    	invertTree(root.left);
+    	invertTree(root.right);
+        return root;
+    }
 	
 	public int maxDepth (TreeNode root){
 
