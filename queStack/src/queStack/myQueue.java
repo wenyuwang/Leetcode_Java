f372ccae1321c9d9ab809aecc3b014f9e8900ded
Wen-Yu, Wang <ww424@cornell.edu>
2015-10-11 13:22:37 -0400

    no message

---
+++ queStack/src/queStack/myQueue.java
@@ -0,0 +1,30 @@
+package queStack;
+
+
+/**Implement the following operations of a queue using stacks.
+ * push(x) -- Push element x to the back of queue.
+ * pop() -- Removes the element from in front of queue.
+ * peek() -- Get the front element.
+ * empty() -- Return whether the queue is empty.
+**/
+public class myQueue {
+    // Push element x to the back of queue.
+    public void push(int x) {
+        
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        
+    }
+
+    // Get the front element.
+    public int peek() {
+        return 0;
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return false;
+    }
+}
