fb6984b77e08790ffe2177b223a78f194a69b5b6
Wen-Yu, Wang <ww424@cornell.edu>
2015-10-15 09:24:15 -0400

    triangle II

--- PascalTriangle/src/TraingleI.java
+++ PascalTriangle/src/TraingleI.java
@@ -1,6 +1,21 @@
 
 import java.util.ArrayList;
 
+/*I.
+ * Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
 public class TraingleI {
     public ArrayList<ArrayList<Integer>> generateI(int numRows) {
         ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
@@ -18,6 +33,20 @@ public class TraingleI {
         		ret.add(cur);
         	}     	
       
+    	return ret;
+    }
+    
+    public ArrayList<Integer> getRow(int rowIndex) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+    	if (rowIndex < 0) return ret;
+        ret.add(1);
+        for (int i =1 ; i<=rowIndex;i++){
+        	for (int j = ret.size()-2; j>=0;j--){
+        		ret.set(j+1, ret.get(j)+ret.get(j+1));
+        	}
+        	ret.add(1);
+        }
+        
     	return ret;
     }
 }
