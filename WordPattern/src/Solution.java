849156c32d5cc6529df63dddb67b3c7a139d5545
Wen-Yu, Wang <ww424@cornell.edu>
2015-10-24 00:35:33 -0400

    no message

--- WordPattern/src/Solution.java
+++ WordPattern/src/Solution.java
@@ -1,4 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Solution {
-
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (pattern.length() != words.length ) return false;
+        Map index = new HashMap<>();
+        for (int i =0; i <words.length; i++){
+        	if (!Objects.equals(index.put(words[i], i), index.put(pattern.charAt(i), i)))
+        		return false;
+        }
+        return true;
+    }
 }
