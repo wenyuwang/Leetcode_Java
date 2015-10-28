b7fe14b49dc8bdb37f570464fddc02c1489e0fcb
Wen-Yu, Wang <ww424@cornell.edu>
2015-10-28 00:30:26 -0400

    no message

--- SingleNumber/src/Solution.java
+++ SingleNumber/src/Solution.java
@@ -1,4 +1,22 @@
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Solution {
-
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i : nums){
+        	if (!set.add(i))
+        		set.remove(i);
+        }
+        Iterator<Integer> it = set.iterator();
+        return it.next();
+    }
+    
+    public int singleNumber2(int[] nums) {
+       int x =0;
+       for (int i : nums){
+    	   x = x ^ i;
+       }
+       return x;
+    }
 }
