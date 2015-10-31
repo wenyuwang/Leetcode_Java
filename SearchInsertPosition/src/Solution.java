c719491849141cb64393fa4f763c8aac636171f0
Wen-Yu, Wang <ww424@cornell.edu>
2015-10-31 16:05:12 -0400

    no message

--- SearchInsertPosition/src/Solution.java
+++ SearchInsertPosition/src/Solution.java
@@ -1,4 +1,24 @@
 
 public class Solution {
-
+    public int searchInsert(int[] nums, int target) {
+        
+    	int left = 0; int right = nums.length-1;
+    	
+    	while (left <= right){
+    		int mid = (left + right)/2;
+    		if (nums[mid] == target) return mid;
+    		if ( mid >1 && nums[mid] < target && nums[mid+1] > target) return mid;
+    		
+    		if (target > nums[mid]){
+    			left = mid +1;
+    		}else{
+    			right = mid -1;
+    		}
+    			
+    		
+    	}
+    	
+    	
+    	return left;
+    }
 }
