00be700b692dc4ad2ba1664f1fad443fb912ec8b
Wen-Yu, Wang <ww424@cornell.edu>
2015-11-03 02:20:33 -0500

    no message

--- SingleNumberII/src/Solution.java
+++ SingleNumberII/src/Solution.java
@@ -1,4 +1,16 @@
 
 public class Solution {
-
+    public int singleNumber(int[] nums) {
+    	int ones=0, twos=0,threes=0;
+    	for(int i =0 ;i<nums.length;i++){
+    		twos |= ones & nums[i];
+    		ones ^= nums[i];
+    		threes = twos & ones;
+    		
+    		ones &= ~threes;
+    		twos &= ~threes;
+    	}
+    	return ones;
+    }
+    
 }
