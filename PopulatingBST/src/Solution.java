e37c791cdca6358b97f2445ac06629fbf1f05b1e
Wen-Yu, Wang <ww424@cornell.edu>
2015-11-03 01:23:51 -0500

    no message

--- PopulatingBST/src/Solution.java
+++ PopulatingBST/src/Solution.java
@@ -1,4 +1,21 @@
 
 public class Solution {
+	public void connect(TreeLinkNode root) {
+		if (root == null) return ;
+		
+		if (root.left != null){
+			root.left.next = root.right;
+		}
+		if (root.right != null && root.next!=null){
+			root.right.next = root.next.left;
+		}
+		connect(root.left);
+		connect(root.right);
 
+	}
+	public class TreeLinkNode {
+		int val;
+		TreeLinkNode left, right, next;
+		TreeLinkNode(int x) { val = x; }
+	}
 }
