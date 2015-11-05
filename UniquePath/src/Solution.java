6d40e8abe1f068bcbf4755998027846caae254ff
Wen-Yu, Wang <ww424@cornell.edu>
2015-11-04 20:39:34 -0500

    no message

--- UniquePath/src/Solution.java
+++ UniquePath/src/Solution.java
@@ -1,14 +1,18 @@
 
 public class Solution {
     public int uniquePaths(int m, int n) {
+    	
+    	// 總共要走 (m-1) + (n-1)步
+    	// 排組
         
     	if (m==1 || n == 1) return 1;
     	m--;
     	n--;
     	if (m < n) {
-    		m = m + n;
-            n = m - n;
-            m = m - n;
+    		int temp = m ;
+    		m = n;
+    		n = temp;
+    		
     	}
     	
     	long ret = 1;
