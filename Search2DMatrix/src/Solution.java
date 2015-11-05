7dcbf1388da288c8ef82a06e44351cc071840441
Wen-Yu, Wang <ww424@cornell.edu>
2015-11-05 17:40:12 -0500

    no message

--- Search2DMatrix/src/Solution.java
+++ Search2DMatrix/src/Solution.java
@@ -1,20 +1,14 @@
 
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    	
-    	for (int i = 0 ; i < matrix[0].length ; i++){
-    		int low = 0, high = matrix.length-1, mid =0;
-    		while(low < high){
-    			mid = low + (high-low)/2;
-    			if (matrix[i][mid] == target) return true;
-    			if (matrix[i][mid] < target) low = mid -1;
-    			else 
-    				low = mid +1 ;
-    			
-    			
-    		}
-    	}
-    	
-    	return false;
+    	return recursionSearch(matrix,target,0,matrix.length*matrix[0].length-1);
+    }
+    private boolean recursionSearch(int [][] matrix, int target, int left, int right){
+    	int mid = (left + right ) /2;
+    	int a = matrix[mid/matrix[0].length][mid%matrix[0].length];
+    	if (target == a) return true;
+    	if (left == right ) return false;
+    	if (target < a ) return recursionSearch(matrix, target, left , mid);
+    	else return recursionSearch(matrix,target,mid+1,right);
     }
 }
