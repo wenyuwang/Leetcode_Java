a928f73d6952216a14b84237afc513b547e6f206
Wen-Yu, Wang <ww424@cornell.edu>
2015-11-09 12:02:46 -0500

    CombinationSum I

--- Combination Sum III/src/Solution.java
+++ Combination Sum III/src/Solution.java
@@ -1,4 +1,6 @@
+import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
     public ArrayList<ArrayList<Integer>> combinationSum3(int k, int n) {
@@ -18,5 +20,35 @@ public class Solution {
     		backTrack(lists,list,i+1,k-1,target-i);
     		lists.remove(list.size()-1);
     	}
+    }
+    
+    
+    
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+        return combination(candidates, target, 0);
+    }
+    private ArrayList<ArrayList<Integer>> combination (int[] candidates, int target, int start){
+    	
+    	ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+    	Arrays.sort(candidates);
+    	
+    	for (int i = start; i<candidates.length;i++){
+    		if(candidates[i] < target){
+    			for (ArrayList<Integer> array : combination(candidates,target-candidates[i],i)){
+    				array.add(0,candidates[i]);
+    				ret.add(array);
+    				
+    			}
+    		}else if (candidates[i]== target){
+				ArrayList<Integer> list = new ArrayList<Integer>();
+				list.add(candidates[i]);
+				ret.add(list);
+			}else 
+				break;
+    		
+    	}
+    	
+    	return ret;
+    	
     }
 }
