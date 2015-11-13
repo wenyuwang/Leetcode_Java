9d8150dbc1d4dc2ab44a6ea2843b4dc6901f2f81
Wen-Yu, Wang <ww424@cornell.edu>
2015-11-13 13:08:11 -0500

    no message

---
+++ RangeSumQuery/src/NumArray.java
@@ -0,0 +1,11 @@
+
+public class NumArray {
+	private int[] num ;
+    public NumArray(int[] nums) {
+        num = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        return 0;
+    }
+}
